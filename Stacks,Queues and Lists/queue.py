3fb5823460dfd4312e35b8afc640d7dc236f2c28
luxury <erikbatista42@gmail.com>
2019-04-15 01:25:59 -0700

    finished annotating arrayQueue

--- Stacks,Queues and Lists/queue.py
+++ Stacks,Queues and Lists/queue.py
@@ -69,33 +69,38 @@ class ArrayQueue(object):
 
     def is_empty(self):
         """Return True if this queue is empty, or False otherwise."""
-        # TODO: Check if empty
+        return not self.list
 
     def length(self):
         """Return the number of items in this queue."""
-        # TODO: Count number of items
+        return len(self.list)
 
     def enqueue(self, item):
         """Insert the given item at the back of this queue.
-        Running time: O(???) – Why? [TODO]"""
-        # TODO: Insert given item
+        Running time: O(1) – Appending to the list is constant time"""
+        self.list.append(item)
 
     def front(self):
         """Return the item at the front of this queue without removing it,
         or None if this queue is empty."""
-        # TODO: Return front item, if any
+        if self.is_empty():
+            return None
+        return self.list[0]
 
     def dequeue(self):
         """Remove and return the item at the front of this queue,
         or raise ValueError if this queue is empty.
-        Running time: O(???) – Why? [TODO]"""
+        Running time: O(n) – because it has to iterate from the last element to the first one"""
         # TODO: Remove and return front item, if any
+        if self.is_empty():
+            raise ValueError("Err! Queue is empty! - There's nothing to dequeue.")
+        try:
+            return self.list[0]
+        finally:
+            self.list.pop(0)
 
 
 # Implement LinkedQueue and ArrayQueue above, then change the assignment below
 # to use each of your Queue implementations to verify they each pass all tests
-Queue = LinkedQueue
-# Queue = ArrayQueue
-
-if __name__ == "__main__":
-    print(123)
+# Queue = LinkedQueue
+Queue = ArrayQueue
