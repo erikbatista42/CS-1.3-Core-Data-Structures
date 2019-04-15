f58feb21d3020a65423d8d200137057cdcd7bbfd
luxury <erikbatista42@gmail.com>
2019-04-15 01:06:38 -0700

    completed linkedQueue

--- Stacks,Queues and Lists/queue.py
+++ Stacks,Queues and Lists/queue.py
@@ -21,27 +21,36 @@ class LinkedQueue(object):
 
     def is_empty(self):
         """Return True if this queue is empty, or False otherwise."""
-        # TODO: Check if empty
+        return self.list.is_empty()
 
     def length(self):
         """Return the number of items in this queue."""
-        # TODO: Count number of items
+        return self.list.length()
 
     def enqueue(self, item):
         """Insert the given item at the back of this queue.
         Running time: O(???) – Why? [TODO]"""
         # TODO: Insert given item
+        self.list.append(item)
 
     def front(self):
         """Return the item at the front of this queue without removing it,
         or None if this queue is empty."""
-        # TODO: Return front item, if any
+        if self.is_empty():
+            return None
+        return self.list.head.data
 
     def dequeue(self):
         """Remove and return the item at the front of this queue,
         or raise ValueError if this queue is empty.
         Running time: O(???) – Why? [TODO]"""
         # TODO: Remove and return front item, if any
+        if self.is_empty():
+            raise ValueError("Err! Queue is empty! - There's nothing to dequeue.")
+        try:
+            return self.list.head.data
+        finally:
+            self.list.delete(self.list.head.data)
 
 
 # Implement ArrayQueue below, then change the assignment at the bottom
